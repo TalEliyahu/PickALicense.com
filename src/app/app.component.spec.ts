03e1eb49f61106c197b66e70bcf08284c70bbb67
Bernard K <kagondubernard81@gmail.com>
2017-11-27 22:25:53 +0300

    fix(cleanup): make tests pass.
    Make tests  pass and retain faq_owners.json and faq_users.json
    previously removed.
    
    closes #4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,31 +3,39 @@ import { AppComponent } from './app.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', function () {
-  let de: DebugElement;
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AppComponent ],
+            imports: [HttpClientModule, HttpClientTestingModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+    });
 
-  it('should create component', () => expect(comp).toBeDefined() );
+    it('should create component', () => expect(comp).toBeDefined() );
 
-  it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
-  });
+    it('should have beginning as the state', () => {
+        expect(comp.state).toEqual('beginning');
+    });
+
+    it('should expect clickable link to start the process', () => {
+        fixture.detectChanges();
+        de = fixture.debugElement.query(By.css('#start-button'));
+        el = de.nativeElement;
+        const startText = el.innerText;
+        expect(startText).toMatch(/Ok, Let's start!/i);
+    });
 });
